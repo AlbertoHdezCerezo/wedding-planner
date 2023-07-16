2e4b7d1dd0e5452da3225bea269138666265ab39
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-07-16 19:38:11 +0200

    chore: add timestampz to database types and set it as default datetime type.

--- config/initializers/postgres.rb
+++ config/initializers/postgres.rb
@@ -2,7 +2,16 @@
 
 require "active_record/connection_adapters/postgresql_adapter"
 
-# Forces Rails to use +postgresql+ +timestampz+ datetime type. This type keeps tracks
-# datetime values with a timezone, which makes easier to handle them when converting
-# times to different time zones.
+# Adds a new type +timestampz+ to the set of +postgresql+ adapter kinds
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter
+      NATIVE_DATABASE_TYPES.merge!(timestampz: { name: "timestamptz" })
+    end
+  end
+end
+
+# Sets +Rails+ to use +timestampz+ instead of +datetime+ in the backend to store
+# datetime values. By tracking the time zone we do not need to take care of converting
+# times to a common time zone before persisting.
 ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.datetime_type = :timestampz
