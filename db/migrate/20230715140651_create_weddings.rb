b011a052d0a2cc19522d38f871c0870b50e48c9b
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-07-16 15:18:29 +0200

    feat: add UUID ID to weddings table

--- db/migrate/20230715140651_create_weddings.rb
+++ db/migrate/20230715140651_create_weddings.rb
@@ -1,6 +1,6 @@
 class CreateWeddings < ActiveRecord::Migration[7.0]
   def change
-    create_table :weddings do |t|
+    create_table :weddings, id: :uuid do |t|
       t.date :date
       t.string :description
 
