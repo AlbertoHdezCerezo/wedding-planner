4b8bbcd1aaf98b6782b192db57e6e63818c6e669
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-10-03 20:33:19 +0200

    Add aria-labelledby to action list items wrapper

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,29 +1,37 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < ViewComponent::Base
-  # Helper Concerns
-  include ClassNameHelper
-
   # Data attribute present in +HTML+ view component wrapper element
   VIEWCOMPONENT_ATTRIBUTE = "data-view-component"
 
-  attr_reader :content_tag_args
+  # Helper Concerns
+  include ClassNameHelper
+
+  def self.stimulus_identifier = name.underscore.gsub("/", "--").tr("_", "-")
+
+  def self.generate_id(suffix = nil) = "#{stimulus_identifier}-#{suffix || SecureRandom.uuid}"
+
+  delegate :generate_id, to: :class
+
+  attr_reader :classes, :id, :content_tag_args
 
   def initialize(classes: nil, **system_arguments)
     super
 
     @classes = class_names(default_classes, classes)
+    @id = system_arguments[:id] || generate_id
 
     system_arguments[VIEWCOMPONENT_ATTRIBUTE.to_sym] = true
 
     @content_tag_args = default_content_tag_arguments.merge(system_arguments)
+    @content_tag_args = @content_tag_args.merge({ id: @id }) if @id.present?
     @content_tag_args = @content_tag_args.merge({ class: @classes }) if @classes.present?
   end
 
   def rendered_slots = self.instance_variable_get(:@__vc_set_slots)
 
   def rendered_slots_by_name(slot_name)
-    slots = rendered_slots.dig(slot_name)
+    slots = rendered_slots[slot_name]
 
     return [] if slots.nil?
 
