7ea226c6d9df25d727c7cbbe081eaa9e51d5bc99
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-10-28 16:53:14 +0200

    Add title_component helper

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 module ComponentHelper
-  def icon_component(icon:, size: :base, **system_arguments)
-    render(Common::IconComponent.new(icon, size:, **system_arguments))
-  end
-
   def base_component(tag: :div, **system_arguments, &)
     render(BaseComponent.new(tag:, **system_arguments), &)
   end
 
+  def icon_component(icon:, size: :base, **system_arguments)
+    render(Common::IconComponent.new(icon, size:, **system_arguments))
+  end
+
   def horizontal_description_list_component(separator: "·", **system_arguments, &)
     render(Common::HorizontalDescriptionListComponent.new(separator:, **system_arguments), &)
+  end
+
+  def title_component(title:, **system_arguments, &)
+    render(Common::TitleComponent.new(title:, **system_arguments), &)
   end
 end
