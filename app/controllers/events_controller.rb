ba32fc43127636af44f395efe3cd52e309408ba1
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-11-22 09:48:12 +0100

    Setup new routes for Event creation

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class EventsController < ApplicationController
+  # GET /weddings/<wedding-id>/new
+  def new
+    set_wedding
+    @event = @wedding.events.new
+  end
+
+  def create
+    set_wedding
+    @event = @wedding.events.new(**event_parameters)
+
+    if @event.save
+      flash.now[:notice] = t("controllers.events_controller.create.notice")
+      render status: :created
+    else
+      flash.now[:alert] = t("controllers.events_controller.create.alert")
+      render status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_wedding
+    @wedding = Wedding.find(params[:wedding_id])
+  end
+
+  def event_parameters = EventParameters.new(params[:event])
+end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,12 +3,6 @@
 class SchedulesController < ApplicationController
   # GET /weddings/<wedding-id>/schedule
   def show
-    set_wedding
-  end
-
-  private
-
-  def set_wedding
     @wedding = Wedding.find(params[:wedding_id])
   end
 end
