674c5ebc916ca8403af494dfd4d030f4748673f6
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-11-26 12:47:30 +0100

    fix: PG Database update to 16 fixes.
    While updating PG to v16 I faced some problems with the PG gem library.
    
    By installing it with the command:
    
    `gem install pg -- --with-pg-config=/opt/homebrew/opt/libpq/bin/pg_config`
    
    I was able to have the whole system already up and running.

--- db/schema.rb
+++ db/schema.rb
@@ -14,44 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_26_084017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.timestamp "created_at", precision: 6, null: false
-    t.timestamp "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.timestamptz "created_at", precision: 6, null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "event_services", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "event_id", null: false
     t.uuid "service_id", null: false
@@ -75,8 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_26_084017) do
   create_table "guests", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "wedding_id", null: false
     t.timestamptz "invited_at", precision: 6
-    t.timestamptz "cancelled_at", precision: 6
     t.timestamptz "confirmed_at", precision: 6
+    t.timestamptz "cancelled_at", precision: 6
     t.timestamptz "created_at", precision: 6, null: false
     t.timestamptz "updated_at", precision: 6, null: false
     t.string "name", null: false
@@ -109,8 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_26_084017) do
     t.string "name", null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "event_services", "events", name: "event_fk_in_event_services"
   add_foreign_key "event_services", "services", name: "service_fk_in_event_services"
   add_foreign_key "events", "weddings", name: "wedding_fk_in_events"
