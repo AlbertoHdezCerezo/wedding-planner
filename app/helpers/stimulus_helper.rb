b5ec9754811ddc9a5cd7ca1855bec180b7aa600e
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-15 07:36:55 +0100

    First successful navigation between invitation postcard pages.
    Implemented flow between welcome and schedule pages.

--- app/helpers/stimulus_helper.rb
+++ app/helpers/stimulus_helper.rb
@@ -5,30 +5,38 @@ module StimulusHelper
     { "data-action" => stimulus_action_value(event, controller_identifier, action) }
   end
 
-  def stimulus_css_class_option(controller_identifier, class_name, css_class)
-    { stimulus_css_class_key(controller_identifier, class_name) => css_class }
-  end
-
-  def stimulus_target_option(name, controller_identifier)
-    { stimulus_target_key(controller_identifier) => name }
-  end
-
-  def stimulus_value_option(controller_identifier, value_name, value)
-    { stimulus_value_key(controller_identifier, value_name) => value }
+  def stimulus_action_value(event, controller_identifier, action)
+    "#{event}->#{controller_identifier}##{action}"
   end
 
   def stimulus_css_class_key(controller_identifier, class_name)
     "data-#{controller_identifier}-#{class_name}-class"
   end
 
+  def stimulus_css_class_option(controller_identifier, class_name, css_class)
+    { stimulus_css_class_key(controller_identifier, class_name) => css_class }
+  end
+
+  def stimulus_parameter_option(name, controller_identifier, value)
+    { stimulus_parameter_key(name, controller_identifier) => value }
+  end
+
+  def stimulus_parameter_key(name, controller_identifier)
+    "data-#{controller_identifier}-#{dasherize(name)}-param"
+  end
+
+  def stimulus_target_option(name, controller_identifier)
+    { stimulus_target_key(controller_identifier) => name }
+  end
+
   def stimulus_target_key(controller_identifier) = "data-#{controller_identifier}-target"
+
+  def stimulus_value_option(controller_identifier, value_name, value)
+    { stimulus_value_key(controller_identifier, value_name) => value }
+  end
 
   def stimulus_value_key(controller_identifier, value_name)
     "data-#{controller_identifier}-#{dasherize(value_name)}-value"
-  end
-
-  def stimulus_action_value(event, controller_identifier, action)
-    "#{event}->#{controller_identifier}##{action}"
   end
 
   private
