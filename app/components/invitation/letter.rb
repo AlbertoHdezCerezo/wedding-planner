9b92d2dd633f29d37bd6b2da2247baffa438c5ba
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-16 07:39:17 +0100

    Implement letter JS controller with flip logic & attach flip action to letter component

--- app/components/invitation/letter.rb
+++ app/components/invitation/letter.rb
@@ -8,4 +8,12 @@ class Invitation::Letter < ApplicationComponent
     @guest = guest
     super(**system_arguments)
   end
+
+  private
+
+  def default_content_tag_arguments
+    options = ::Html::TagAttributes.build(super, { style: "perspective: 1600px" })
+    options = options.with_stimulus_controller(self.class.stimulus_identifier)
+    options.to_h
+  end
 end
