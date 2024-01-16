012a6bc4b0b7af52ab953fa7a981ab1853e4bae8
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-16 07:39:17 +0100

    Implement letter JS controller with flip logic & attach flip action to letter component

---
+++ app/components/invitation/letter_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from '@hotwired/stimulus'
+import anime from 'animejs'
+
+export default class extends Controller {
+  // Pivot element for rotation
+  static targets = ['innerLetter']
+
+  // Tracks flip state. If true it means the back of the letter faces the screen
+  flipped = false
+  // AnimeJS timeline instance
+  timeline = null
+
+  async flip () {
+    if (this.timeline) return
+
+    this.timeline = anime
+      .timeline({ easing: 'easeInQuad', duration: 1400, autoplay: false })
+      .add({ targets: this.innerLetterTarget, rotateY: this.flipped ? '0deg' : '180deg' })
+
+    this.timeline.play()
+
+    await this.timeline.finished
+
+    console.log('me llama')
+    this.timeline = null
+    this.flipped = !this.flipped
+  }
+}
