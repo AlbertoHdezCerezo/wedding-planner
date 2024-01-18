966a3a75a7f4a320122cd461399b4988e847384b
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-18 10:54:07 +0100

    chore: lint

--- app/components/invitation/letter.rb
+++ app/components/invitation/letter.rb
@@ -12,7 +12,9 @@ class Invitation::Letter < ApplicationComponent
   private
 
   def default_content_tag_arguments
-    options = ::Html::TagAttributes.build({ class: "absolute top-[100%] left-0 h-full w-full overflow-y-scroll w-fit pt-[20%] pb-[20%]" })
+    options = ::Html::TagAttributes.build(
+      { class: "absolute top-[100%] left-0 h-full w-full overflow-y-scroll w-fit pt-[20%] pb-[20%]" }
+    )
     options = options.with_stimulus_controller(stimulus_identifier)
     options.to_h
   end
