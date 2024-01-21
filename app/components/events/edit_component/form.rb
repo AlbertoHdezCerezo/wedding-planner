8f8af4453ea2fc1f328fa02a931ef243553ca1f1
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-21 15:46:02 +0100

    Extend Event form with Place & Address fields

--- app/components/events/edit_component/form.rb
+++ app/components/events/edit_component/form.rb
@@ -15,7 +15,7 @@ class Events::EditComponent::Form < ApplicationComponent
   def default_content_tag_arguments
     {
       id: ID,
-      class: "flex flex-col gap-6 px-8 py-10"
+      class: "max-h-[70vh] overflow-y-auto flex flex-col gap-6 px-8 py-10"
     }
   end
 end
