e8ad3c6989a13ac3502720dfa3269e20e3976d4d
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-21 16:05:38 +0100

    Add validations for nested associations between Events, Places and Addresses.

--- app/models/event_place.rb
+++ app/models/event_place.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class EventPlace < ApplicationRecord
+  # Associations
   belongs_to :event
   belongs_to :place
+
+  accepts_nested_attributes_for :place, allow_destroy: true
+
+  # Validations
+  validates_associated :place
 end
