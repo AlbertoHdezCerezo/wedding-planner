46adf2b19608a0099979bb0c8537346a070c94d7
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-23 14:33:32 +0100

    Add layout for invitation schedule page

--- test/components/previews/invitation/show_component_preview.rb
+++ test/components/previews/invitation/show_component_preview.rb
@@ -1,10 +1,37 @@
 # frozen_string_literal: true
 
 class Invitation::ShowComponentPreview < ViewComponent::Preview
+  def self.wedding
+    date = Date.new(2024, 9, 14)
+
+    wedding = FactoryBot.build(:wedding, date:)
+
+    wedding.events << FactoryBot.build(:event, name: "Welcome Cocktail", schedule: (date + 15.hours)..(date + 17.hours), place:)
+    wedding.events << FactoryBot.build(:event, name: "Engagement Ceremony", schedule: (date + 17.hours)..(date + 18.hours), place:)
+    wedding.events << FactoryBot.build(:event, name: "Wedding Dinner", schedule: (date + 18.hours)..(date + 20.hours), place:)
+    wedding.events << FactoryBot.build(:event, name: "Dance Party", schedule: (date + 20.hours)..(date + 24.hours), place:)
+
+    wedding
+  end
+
+  def self.place
+    FactoryBot.build(
+      :place,
+      name: "Parador de Plasencia",
+      address: FactoryBot.build(
+        :address,
+        street: "Plaza San Vicente Ferrer, 5",
+        town: "Plasencia",
+        country: "Spain",
+        zip_code: "10600"
+      )
+    )
+  end
+
   layout "application"
 
   def default
-    wedding = FactoryBot.build(:wedding)
+    wedding = self.class.wedding
     guest = FactoryBot.build(:guest)
     render(Invitation::ShowComponent.new(wedding:, guest:))
   end
