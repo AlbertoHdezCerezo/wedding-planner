ba1307d7b31399336779ff5767e1e9ed6928e5f6
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-24 19:08:55 +0100

    fix: tests

--- lib/factories/invitations.rb
+++ lib/factories/invitations.rb
@@ -2,7 +2,14 @@
 
 FactoryBot.define do
   factory :invitation do
-    association :wedding
-    guests { build_list(:guest, 1, wedding:) }
+    # For +Invitation+ factory to create invitations with +Guests+ being part
+    # of +Wedding+ +Guests+ list we need to previously create both records
+    transient do
+      sample_guest { create(:guest) }
+      sample_wedding { create(:wedding, guests: [sample_guest]) }
+    end
+
+    wedding { sample_wedding }
+    guests { [sample_guest] }
   end
 end
