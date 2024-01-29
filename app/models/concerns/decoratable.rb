c9e2ae4419f3e189ddd06487140786c0c0ef45b7
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-01-29 16:38:46 +0100

    Rename Decorator concern to Decoratable. Add test coverage for Decoratable concern.

--- app/models/concerns/decoratable.rb
+++ app/models/concerns/decoratable.rb
@@ -1,12 +1,14 @@
 
 module Decoratable
+  class DecoratorNoDefinedError < StandardError; end
+
   extend ActiveSupport::Concern
 
   included do
     def decorator
       @decorator ||= "#{self.class.name}Decorator".constantize.new(self)
     rescue NameError
-      raise <<-EXCEPTION
+      raise DecoratorNoDefinedError, <<-EXCEPTION
         There is no #{self.class.name}Decorator class defined for #{self.class.name} model.
       EXCEPTION
     end
