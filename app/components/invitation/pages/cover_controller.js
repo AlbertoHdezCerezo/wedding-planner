1f28281053b241cc0411b94cadfbdae27bd0c772
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-05 09:46:44 +0100

    Add transition to calendar button when user accepts invitation

--- app/components/invitation/pages/cover_controller.js
+++ app/components/invitation/pages/cover_controller.js
@@ -1,5 +1,20 @@
+import { Animator } from '../../../javascript/src/invitation/animator'
 import { Controller } from '@hotwired/stimulus'
 import JSConfetti from 'js-confetti'
+
+const BEFORE_ACCEPT_ANIMATION = {
+  autoplay: false,
+  duration: 800,
+  easing: 'easeInExpo',
+  opacity: [1, 0]
+}
+
+const AFTER_ACCEPT_ANIMATION = {
+  autoplay: false,
+  duration: 800,
+  easing: 'easeInExpo',
+  opacity: [0, 1]
+}
 
 /**
  * Small controller which listens for invitation confirmation form
@@ -7,11 +22,17 @@ import JSConfetti from 'js-confetti'
  * displays the button to save the event to the user's calendar.
  */
 export default class extends Controller {
-  static targets = ['acceptButton', 'eventButton']
+  static targets = [
+    // Target/s whose content is displayed before the invitation is accepted
+    'beforeAccept',
+    // Target/s whose content is displayed after the invitation is accepted
+    'afterAccept'
+  ]
 
   greetConfirmedGuest ({ detail: { fetchResponse } }) {
     if (fetchResponse.succeeded) {
       this.#throwConfetti()
+      this.#switchConfirmAndCalendarButtons()
     }
   }
 
@@ -19,5 +40,25 @@ export default class extends Controller {
     const confetti = new JSConfetti()
     await confetti.addConfetti({ emojis: ['🔔', '💒', '🥂', '✨', '💍', '🍾'] })
     confetti.clearCanvas()
+  }
+
+  async #switchConfirmAndCalendarButtons () {
+    await Animator.play(
+      Animator.animation({
+        targets: this.beforeAcceptTarget,
+        ...BEFORE_ACCEPT_ANIMATION
+      })
+    )
+
+    // Remove the before-accept content from the DOM and reveal the after-accept content
+    this.beforeAcceptTarget.remove()
+    this.afterAcceptTarget.classList.remove('hidden')
+
+    await Animator.play(
+      Animator.animation({
+        targets: this.afterAcceptTarget,
+        ...AFTER_ACCEPT_ANIMATION
+      })
+    )
   }
 }
--- app/components/invitation/pages/page.rb
+++ app/components/invitation/pages/page.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Invitation::Pages::Page < ApplicationComponent
+  attr_reader :preview_mode
+
   def self.page_name = raise "Page Component #{name} does not define page_name method"
 end
