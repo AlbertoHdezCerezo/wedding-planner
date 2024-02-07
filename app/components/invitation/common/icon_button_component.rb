030ac35345ef86f1211cbcf4c114f01713a99ca9
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-07 10:58:46 +0100

    Style Invitation navigation buttons

--- app/components/invitation/common/icon_button_component.rb
+++ app/components/invitation/common/icon_button_component.rb
@@ -2,5 +2,18 @@
 
 module Invitation::Common
   class IconButtonComponent < Common::IconButtonComponent
+    SCHEMES = { cardboard: "
+      bg-transparent text-gray-500 border-[1px] border-gray-200 opacity-60
+      hover:opacity-100 hover:bg-gray-100
+      active:opacity-100 active:bg-gray-100
+      focus:opacity-100 focus:bg-gray-100
+    " }.freeze
+
+    protected
+
+    def default_content_tag_arguments
+      options = { class: "uppercase !text-[10px] tracking-[.05em] #{SCHEMES[scheme] if SCHEMES.key?(scheme)}" }
+      ::Html::TagAttributes.build(super, options).to_h
+    end
   end
 end
