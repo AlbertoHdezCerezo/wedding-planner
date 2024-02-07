a61a2f9e39724a3ca4038d9a13abf13b438d8f59
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-07 10:07:59 +0100

    Updated Invitation ButtonComponent to define new schemas designed only for the Invitation.

--- app/components/invitation/common/button_component.rb
+++ app/components/invitation/common/button_component.rb
@@ -2,10 +2,16 @@
 
 module Invitation::Common
   class ButtonComponent < Common::ButtonComponent
+    SCHEMES = { cardboard: "
+      bg-transparent text-gray-900 border-[1px] border-gray-200 opacity-60
+      hover:opacity-100 active:opacity-100 focus:opacity-100
+    " }.freeze
+
     protected
 
     def default_content_tag_arguments
-      ::Html::TagAttributes.build(super, { class: "uppercase !text-[10px] tracking-[.05em]" }).to_h
+      options = { class: "uppercase !text-[10px] tracking-[.05em] #{SCHEMES[scheme] if SCHEMES.key?(scheme)}" }
+      ::Html::TagAttributes.build(super, options).to_h
     end
   end
 end
