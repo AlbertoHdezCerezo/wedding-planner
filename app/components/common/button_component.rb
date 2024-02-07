3435a70f7a4cf616ed86a5f7be6ead8101458abb
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-07 09:13:48 +0100

    refactor: button component data-scoped classes replaced by inline classes
    The previous approach of using the data attribute to set different button scheme and sizes bloatted the DOM with Tailwind classes.
    
    We only want to switch the style of a button when it is enabled or disabled, thus it has no sense to base the style in these other data attributes.

--- app/components/common/button_component.rb
+++ app/components/common/button_component.rb
@@ -3,17 +3,33 @@
 module Common
   class ButtonComponent < ApplicationComponent
     SIZES = {
-      small: "small",
-      medium: "medium",
-      large: "large"
+      small: "h-9 px-4 text-xs",
+      medium: "h-11 px-5 text-base",
+      large: "h-14 px-6 text-lg"
     }.freeze
 
     SCHEMES = {
-      primary: "primary",
-      secondary: "secondary",
-      invisible: "invisible",
-      danger: "danger",
-      danger_invisible: "danger-invisible"
+      primary: "
+        bg-gray-700 text-white
+        hover:bg-gray-950 active:bg-black focus:bg-gray-950
+      ",
+      secondary: "
+        bg-transparent text-gray-900 border-[1px] border-gray-200
+        hover:bg-gray-100 active:bg-gray-50 focus:bg-gray-100
+      ",
+      invisible: "
+        bg-transparent text-gray-800
+        hover:bg-gray-100 active:bg-gray-200 focus:bg-gray-200
+      ",
+      danger: "
+        bg-cerise-500 text-white
+        hover:bg-cerise-400 active:bg-cerise-300 focus:bg-cerise-300
+      ",
+      danger_invisible: "
+        bg-transparent text-cerise-400
+        hover:bg-cerise-700 active:bg-cerise-600 focus:bg-cerise-600
+        hover:text-white active:text-white focus:text-white
+      "
     }.freeze
 
     attr_reader :label, :size, :scheme
@@ -36,33 +52,11 @@ module Common
     def default_content_tag_arguments
       {
         tag: :button,
-        data: {
-          size: SIZES[size],
-          scheme: SCHEMES[scheme]
-        },
         class: <<-HTML
+          #{SIZES[size]}
+          #{SCHEMES[scheme]}
+
           w-fit font-ibm font-medium rounded-full cursor-pointer flex items-center justify-center
-
-          data-[size=small]:h-9 data-[size=small]:px-4 data-[size=small]:text-xs
-          data-[size=medium]:h-11 data-[size=medium]:px-5 data-[size=medium]:text-base
-          data-[size=large]:h-14 data-[size=large]:px-6 data-[size=large]:text-lg
-
-          data-[scheme=primary]:bg-gray-700 data-[scheme=primary]:text-white
-          data-[scheme=primary]:hover:bg-gray-950 data-[scheme=primary]:active:bg-black data-[scheme=primary]:focus:bg-gray-950
-
-          data-[scheme=secondary]:bg-white data-[scheme=secondary]:text-gray-900 data-[scheme=secondary]:border-[1px] data-[scheme=secondary]:border-gray-200
-          data-[scheme=secondary]:hover:bg-gray-100 data-[scheme=secondary]:active:bg-gray-50 data-[scheme=secondary]:focus:bg-gray-100
-
-          data-[scheme=invisible]:bg-transparent data-[scheme=invisible]:text-gray-800
-          data-[scheme=invisible]:hover:bg-gray-100 data-[scheme=invisible]:active:bg-gray-200 data-[scheme=invisible]:focus:bg-gray-200
-
-          data-[scheme=danger]:bg-cerise-500 data-[scheme=danger]:text-white
-          data-[scheme=danger]:hover:bg-cerise-400 data-[scheme=danger]:active:bg-cerise-300 data-[scheme=danger]:focus:bg-cerise-300
-
-          data-[scheme=danger-invisible]:bg-transparent data-[scheme=danger-invisible]:text-cerise-400
-          data-[scheme=danger-invisible]:hover:bg-cerise-700 data-[scheme=danger-invisible]:active:bg-cerise-600 data-[scheme=danger-invisible]:focus:bg-cerise-600
-          data-[scheme=danger-invisible]:hover:text-white data-[scheme=danger-invisible]:active:text-white data-[scheme=danger-invisible]:focus:text-white
-
           disabled:opacity-60 disabled:pointer-events-none
         HTML
       }
