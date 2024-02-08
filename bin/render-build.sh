59640a4c21d1931fa05081001c667f16ecf2feac
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-08 12:16:00 +0100

    Add Render launch script and configuration specification

---
+++ bin/render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Script to build project & deploy in Render
+# ------------------------------------------
+
+# exit on error
+set -o errexit
+
+# 1. Install dependencies
+bundle install
+
+# Compile assets
+./bin/rails assets:precompile
+./bin/rails assets:clean
