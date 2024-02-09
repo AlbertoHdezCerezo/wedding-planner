d5f9b6a0ddca036cdb702e154f2ac1661a911635
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-09 18:44:37 +0100

    Adjusted responsive layout for envelop. Now the reveal of the card works on all different devices.

--- app/components/invitation/layout/page_component.rb
+++ app/components/invitation/layout/page_component.rb
@@ -4,7 +4,9 @@ class Invitation::Layout::PageComponent < Invitation::ApplicationComponent
   def default_content_tag_arguments
     {
       class: <<-HTML
-        h-[520px] w-[350px] max-h-[85%] max-w-[90%] m-auto overflow-y-scroll
+        h-[520px] w-[350px]
+        landscape:max-lg:!w-[60vw] landscape:max-lg:!h-[60vh]
+        m-auto overflow-y-scroll
       HTML
     }
   end
