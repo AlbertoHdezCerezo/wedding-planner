ecb427faec9247d8a4d840b3627d5457815fb0f5
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-23 20:41:37 +0100

    fix: some adjustments

--- app/components/invitation/layout/page_component.rb
+++ app/components/invitation/layout/page_component.rb
@@ -4,7 +4,7 @@ class Invitation::Layout::PageComponent < Invitation::ApplicationComponent
   def default_content_tag_arguments
     {
       class: <<-HTML
-        h-[520px] w-[350px] max-w-[85vw] max-[361px]:max-h-[65vh]
+        h-[520px] w-[350px] max-w-[85vw] max-[361px]:max-h-[60vh]
         m-auto overflow-y-scroll
       HTML
     }
